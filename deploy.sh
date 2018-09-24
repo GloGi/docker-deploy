ccf16b2a06cd5244065504fe2a5472fc921a6c31
Jussi Viitamäki <jussi@kwd.fi>
2018-09-24 11:44:22 +0300

    Changed docker_env -> service_group, added support for app_name and removal of image before deploy.

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if [ -z ${2+x} ]; then
 fi
 
 MAPPING="./config/mapping.yml"
-DEPLOY_KEYS=("ssh_config" "compose_file" "docker_env")
+DEPLOY_KEYS=("ssh_config" "compose_file" "service_group")
 PROJECT="$1"
 ENV="$2"
 RESTART_DOCKER="$3"
@@ -35,9 +35,22 @@ fi
 BASE="sites_${PROJECT}_${ENV}"
 SSH_CONFIG="${BASE}_ssh_config"
 COMPOSE_FILE="${BASE}_compose_file"
-DOCKER_ENV="${BASE}_docker_env"
+SERVICE_GROUP="${BASE}_service_group"
 
-COMMAND="docker deploy -c ${!COMPOSE_FILE} ${!DOCKER_ENV} --with-registry-auth"
+COMMAND=""
+if [ "$RESTART_DOCKER" == "restart" ];
+    then
+        SERVICE_NAME="${BASE}_app_name"
+
+        if [ -z ${!SERVICE_NAME+x} ]; then
+            echo "Application name missing"
+            exit 1
+        fi
+
+        COMMAND="docker service rm ${!SERVICE_NAME}; "
+fi
+
+COMMAND+="docker deploy -c ${!COMPOSE_FILE} ${!SERVICE_GROUP} --with-registry-auth"
 
 read -p "You are about to execute command: ${COMMAND}. Do you want to continue (y/N) " INPUT
 
@@ -48,4 +61,3 @@ if [ "$INPUT" != "y" -a "$INPUT" != "Y" ];
     else
         eval $COMMAND
 fi
-
